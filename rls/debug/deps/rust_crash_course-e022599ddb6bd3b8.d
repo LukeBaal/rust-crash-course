b6bd3d6cc712df49dcb8331fac2231d6d9d49b5a
LukeBaal <luke.baal@uoit.net>
2019-02-21 09:27:31 -0500

    Added files

---
+++ rls/debug/deps/rust_crash_course-e022599ddb6bd3b8.d
@@ -0,0 +1,6 @@
+/home/luke/Documents/rust-crash-course/rls/debug/deps/rust_crash_course-e022599ddb6bd3b8.rmeta: src/main.rs src/print.rs
+
+/home/luke/Documents/rust-crash-course/rls/debug/deps/rust_crash_course-e022599ddb6bd3b8.d: src/main.rs src/print.rs
+
+src/main.rs:
+src/print.rs:
---
+++ src/print.rs
@@ -0,0 +1,10 @@
+pub fn run() {
+  // Print to console
+  println!("Hello from the print.rs file");
+
+  // Basic Formatting
+  println!("{} is from {}", "Luke", "ON");
+
+  // Positional Arguments
+  println!("{} is from {} and {} likes to {}")
+}
